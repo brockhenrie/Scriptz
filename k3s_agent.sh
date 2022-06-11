ff828c8470e31a6222cbc74e80e2917f4fa5c590
brock <bhenriedev@gmail.com>
2022-06-11 02:56:50 +0000

    k3s scripts for ubuntu init

---
+++ k3s_agent.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+serverIp=10.10.10.70
+token=B@ckspace1B@ckspace1964
+
+echo "Updating and upgrading apt!"
+sudo apt update -y
+
+sudo apt upgrade -y
+
+echo "Setting firewall"
+
+sudo ufw enable
+
+sudo ufw default allow outgoing
+
+sudo ufw deny incoming
+
+sudo ufw allow ssh
+
+sudo ufw allow 443
+
+sudo ufw allow 80
+
+sudo ufw allow 6443
+sudo ufw allow 8472
+sudo ufw allow 51820
+sudo ufw allow 51821
+sudo ufw allow 10250
+sudo ufw allow 2379
+sudo ufw allow 2380
+
+sudo apt install unnattended-upgrades -y
+
+curl -sfL https://get.k3s.io | K3S_TOKEN=$token K3S_URL=https://$serverIp:6443 sh -
